6f8e8db24d4eee204567cef9410109ce3bd71872
tung181299 <vuongtochoang@gmail.com>
2021-05-20 01:02:22 +0700

    Update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Register from './Register';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
